cc4045bc8d026bb9f12825d157555f3578618b04
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-07-18 17:27:54 +0530

    Day 2 : Variables & Data Types

---
+++ day2_of_python.py
@@ -0,0 +1,16 @@
+# Day 2 : Variables & Data Types
+
+name = "Aryan Bhagwat"  # This is a string variable
+age = 20  # This is an integer variable
+height = 6.0 # This is a float variable
+is_student = True #This is a boolean variable
+
+print("My name is", name) # This prints my name
+print("I am", age, "years old") # This prints my age
+print("My height is", height, "feet") # This prints my height
+print("Am i a student?", is_student) # This prints if I am a student
+
+print(type(name)) #This print the type of the variable name
+print(type(age)) #This print the type of the variable age 
+print(type(height)) #This print the type of the variable height
+print(type(is_student)) #This print the type of the variable is_student
