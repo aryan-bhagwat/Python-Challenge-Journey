b2b0a776279bb0215ffda274407c96f2d7c997d3
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-07-18 17:14:00 +0530

    Day 1 : Python Setup, Hello World, and Comments

---
+++ day1_of_python.py
@@ -0,0 +1,7 @@
+# Day 1 : Python Setup, Hello World, and Comments
+
+print("Hello, World!") # This prints a line of text
+
+print("I'm Aryan Bhagwat") # This prints my name
+print("I'm 20 years old") # This prints my age
+print("My goal is to become a Python Expert") # This prints my goal
