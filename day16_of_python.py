017644447e803768f1ff26d79c23300e0c79814c
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-07-22 13:06:17 +0530

    Day 16 : Dictionary Comprehension in Python

--- day16_of_python.py
+++ day16_of_python.py
@@ -1,50 +1,49 @@
 # Day 16 : Dictionary Comprehension in Python
-'''
+
+## Example 1: Convert a List to Dictionary
 names = ["Aryan", "Sana", "Ravi"]
 marks = [85, 76, 92]
 
 student_marks = {names[i]: marks[i] for i in range(len(names))}
 print(student_marks)
 
-
+## Example 2: Add Conditional Logic
 nums = range(1, 6)
 even_squares = {x: x**2 for x in nums if x % 2 == 0}
 print(even_squares)  # Output: {2: 4, 4: 16}
 
+## Advanced Use: String Counter
 word = "banana"
 char_word = {char : word.count(char) for char in set(word)}
 print(char_word)
 
-names = ["Aryan", "Sana", "Ravi"]
-marks = [85, 76, 92]
 
-name_marks = {names[i] : marks[i] for i in range(len(names))}
-print(name_marks)
+## Basic Practice
 
-
-# Create a dictionary with numbers as keys and their cubes as values from 1 to 5.
+# 1. Create a dictionary with numbers as keys and their cubes as values from 1 to 5.
 cube = {x : x**3 for x in range(1,6)}
 print(cube)
 
-# Build a dictionary of student names and their lengths.
+# 2. Build a dictionary of student names and their lengths.
 stu_names = {x : len(x) for x in names}
 print(stu_names)
 
 
-# Convert a list of tuples to a dictionary:
+## Intermediate Practice
+
+# 1. Convert a list of tuples to a dictionary:
 data = [("a", 1), ("b", 2), ("c", 3)]
 
 tuple_to_dic = {i[0] : i[1] for i in data}
 print(tuple_to_dic)
 
 
-# From a dictionary of student marks: Create a new dict with only students who passed (marks >= 60).
-
+# 2. From a dictionary of student marks: Create a new dict with only students who passed (marks >= 60).
 students = {"Aryan": 85, "Ravi": 45, "Sana": 76}
 
 pass_students = {name : mark for name, mark in students.items() if mark >= 60}
 print(pass_students)
-'''
+
 
 ## Bonus Challenge : Grade Mapping System
 
