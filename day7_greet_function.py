81a022382d8f27773d0104447a67bb6a41ef94ae
Aryan Dnyandev Bhagwat <aryanbhagwat90@gmail.com>
2025-07-27 16:58:21 +0530

    Delete day7_greet_function.py

--- day7_greet_function.py
+++
@@ -1,8 +0,0 @@
-# Day 7 Challenge: Create a reusable greeting function
-
-def greet_user(name):
-    print(f"Hello, {name}! Hope you're having a great day!")
-
-# Get input and call the function
-user_name = input("Enter your name: ")
-greet_user(user_name)
