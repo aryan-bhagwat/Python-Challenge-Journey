f86d4694ffcb2131bf5e8999ab888c01dcf38ee3
Aryan Dnyandev Bhagwat <aryanbhagwat90@gmail.com>
2025-07-27 16:56:49 +0530

    Delete day19_dict_comprehension.py

--- day19_dict_comprehension.py
+++
@@ -1,5 +0,0 @@
-# Day 19 Challenge: Create a dictionary with squares of numbers
-numbers = [1, 2, 3, 4, 5]
-squares = {num: num ** 2 for num in numbers}
-
-print("Number-Square Dictionary:", squares)
