478dbf3540658e3f599ea3efd20cd25c9f78acfb
Aryan Dnyandev Bhagwat <aryanbhagwat90@gmail.com>
2025-07-27 16:58:03 +0530

    Delete day17_zip_lists.py

--- day17_zip_lists.py
+++
@@ -1,9 +0,0 @@
-# Day 17 Challenge: Use zip() to pair elements of two lists
-names = ["Aryan", "Dev", "Karan"]
-roles = ["Analyst", "Developer", "Manager"]
-
-paired = list(zip(names, roles))
-
-print("Paired List:")
-for name, role in paired:
-    print(f"{name} - {role}")
