0279ff6e81b83793be1812c3846b58c5c7022c4c
Aryan Dnyandev Bhagwat <aryanbhagwat90@gmail.com>
2025-07-27 16:57:01 +0530

    Delete day1_intro_program.py

--- day1_intro_program.py
+++
@@ -1,7 +0,0 @@
-# Day 1 Challenge: Introduction Program
-
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-language = input("What is your favorite programming language?")
-
-print(f"\nHello {name}! You are {age} years old and you love {language}. Keep going!")
